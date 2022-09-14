a34367baeed5b8bfc4bca9923248b85c76fe90f2
askazakov <askazakov@gmail.com>
2022-09-14 13:51:42 +0500

    inner logging

--- WebApplication1/Controllers/OrderRepository.cs
+++ WebApplication1/Controllers/OrderRepository.cs
@@ -16,21 +16,29 @@ public class OrderRepository
 
     public IEnumerable<OrderResponse> GetOrders(bool throwException)
     {
-        _logger.BeginScope(new Dictionary<string, string>
+        try
         {
-            ["inner_scope_id"] = Guid.NewGuid().ToString()
-        });
-        _logger.LogInformation($"{nameof(GetOrders)} invoked");
-        
-        if (throwException)
-        {
-            throw new ApplicationException("requested exception");
+            _logger.BeginScope(new Dictionary<string, string>
+            {
+                ["inner_scope_id"] = Guid.NewGuid().ToString()
+            });
+            _logger.LogInformation($"{nameof(GetOrders)} invoked");
+
+            if (throwException)
+            {
+                throw new ApplicationException("requested exception");
+            }
+
+            return Enumerable.Range(1, 5).Select(_ => new OrderResponse
+            {
+                OrderId = Guid.NewGuid(),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            });
         }
-        
-        return Enumerable.Range(1, 5).Select(_ => new OrderResponse
+        catch (Exception ex)
         {
-            OrderId = Guid.NewGuid(),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        });
+            _logger.LogError(ex, "inner log entry for exception");
+            throw;
+        }
     }
 }
